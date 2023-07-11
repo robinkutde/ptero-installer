6d589b5087870a231aad7b63047ce766271308c7
Robin <92713851+RobKut999@users.noreply.github.com>
2023-07-11 13:46:37 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+sudo apt update && apt upgrade && sudo apt install curl && sudo bash <(curl -sS https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer/master/install.sh) && sudo systemctl restart nginx && sudo apt update && apt upgrade
